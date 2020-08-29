496aa66dcca7bdbf3c918854f7a0d07b693425fb
Your Name <you@example.com>
2020-08-29 14:43:12 +0000

    changes from server

--- tcp.js
+++ tcp.js
@@ -3,17 +3,26 @@ const express = require('express');
 const app = express();
 // require('./db');
 const https = require('https');
-const DeviceModel = require('./model/device');
+//const DeviceModel = require('./model/device');
 
 // var socket;
 
 var tcp = net.createServer(function (soc) {
     console.log("CONNECTED");
+
     soc.setKeepAlive(true);
+
+
+soc.on('error', function(err) {
+console.log(err);
+});
+
     soc.on('data', async function (data) {
         data = data.toString();
         console.log(data);
 
+soc.write('100');
+//soc.pipe(socket);
         // const Device = new DeviceModel({
         //     deviceID: "Device001",
         //     value: data
