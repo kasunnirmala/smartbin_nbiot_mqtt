741ee8cc05107606fecefffbbf4dd73a859a67b5
Kasun Nirmala <bgkasunnirmala@gmail.com>
2020-10-23 12:44:40 +0530

    byData API added

--- routes/deviceData.js
+++ routes/deviceData.js
@@ -5,7 +5,7 @@ var moment = require('moment-timezone');
 
 router.get('/byDateAndID/:nodeID/:date', async (req, res) => {
     try {
-        var DeviceData = await DeviceDataModel.find({ node_id: req.params.nodeID, date: req.params.date, place:'cocacola' });
+        var DeviceData = await DeviceDataModel.find({ node_id: req.params.nodeID, date: req.params.date, place: 'cocacola' });
         res.json(DeviceData);
     } catch (error) {
         res.json({ message: error.message });
@@ -13,6 +13,25 @@ router.get('/byDateAndID/:nodeID/:date', async (req, res) => {
 
 })
 
+
+router.get('/byData/:date', async (req, res) => {
+    try {
+        var showData = [];
+        var DeviceData = await DeviceDataModel.find({ node_id: "ANANKE002", date: req.params.date, place: 'cocacola' });
+        DeviceData.forEach(deviceData => {
+            showData.push({
+                datetime: deviceData.datetime,
+                value: deviceData.value,
+                color: deviceData.value < 40 ? "RED" : deviceData.value < 80 ? "ORANGE" : deviceData.value < 120 ? "BLUE" : "GREEN"
+            });
+        });
+
+        res.json(showData);
+    } catch (error) {
+        res.json({ message: error.message });
+    }
+
+})
 
 router.get('/getAll', async (req, res) => {
     try {
